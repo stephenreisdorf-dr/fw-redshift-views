27ce6b97534b4548f1f53b1868ef0abde438e28b
Stephen Reisdorf <stephen.reisdorf@daveramsey.com>
2020-06-02 15:33:47 -0500

    Updating opportunities_all and sr_mapping views.

--- salesforce_views/fw_opportunities_all.sql
+++ salesforce_views/fw_opportunities_all.sql
@@ -4,12 +4,6 @@ fo.opportunity_id
 , fo.account_id
 , fa.account
 , fa.client_status
-, fp.payment_id
-, fp.payment_owner_id
-, fp.payment_owner
-, fp.invoice_sent_date
-, fp.invoice_paid_date
-, fp.payment_due_date
 , fo.opportunity_record_type
 , fo.opportunity_owner
 , fo.opportunity_stage
@@ -22,13 +16,6 @@ fo.opportunity_id
 , fo.annual_amount
 , fa.employees
 , fa.number_of_eligible_employees as account_eligible_employees
-, count(*) over (partition by fo.opportunity_id) as total_payments
-, fo.amount / total_payments as amount_per_payment
-, fo.annual_amount / total_payments as annual_amount_per_payment
-, fp.invoiced_amount
-, fp.amount_received
 from analytics_sandbox.fw_opportunities fo
-join analytics_sandbox.fw_accounts fa
+left join analytics_sandbox.fw_accounts fa
 	on fo.account_id = fa.account_id
-join analytics_sandbox.fw_payments fp
-	on fp.opportunity_id = fo.opportunity_id
--- salesforce_views/fw_sr_mapping.sql
+++ salesforce_views/fw_sr_mapping.sql
@@ -1,11 +1,11 @@
 select distinct
 sr.opportunity_id
-, sr.sr_account
+, sr.sr_account_id
 from (
 	select
 	sr.id
 	,sr.client_opportunity__c as opportunity_id
-	,sr.sr_account__c as sr_account
+	,sr.sr_account__c as sr_account_id
 	,case when sr.isdeleted = 'true' then true else false end as is_deleted
 	,row_number() over(partition by sr.id order by sr.lastmodifieddate desc) = 1 as keep
 	from crm_salesforce_strength_parquet.client_related_opportunity__c sr 
